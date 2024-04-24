98197d3f230e39a4e35d8319c68c284bab96c080
Tomiwa <oyindml1@gmail.com>
2024-04-24 12:31:12 +0100

    commit

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -1,3 +1,4 @@
+import 'package:bankingapp/utilities/themeColors.dart';
 import 'package:bankingapp/utilities/themeStyles.dart';
 import 'package:bankingapp/widgets/creditCard.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,29 @@ class CardsList extends StatefulWidget {
 }
 
 class _CardsListState extends State<CardsList> {
+  List cardList = [
+    const CreditCard(),
+    const CreditCard(),
+    const CreditCard(),
+    const CreditCard(),
+    const CreditCard(),
+  ];
+  int _currentCard = 0;
+
+  final PageController _pageController = pageController(initialPage: 0);
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  _onPageChanged(int index) {
+    setState(() {
+      _currentCard = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,8 +49,50 @@ class _CardsListState extends State<CardsList> {
               ],
             ),
           ),
-          const CreditCard()
+          Container(
+            height: 246.0,
+            child: PageView.builder(
+              itemCount: cardList.length,
+              scrollDirection: Axis.horizontal,
+              controller: _pageController,
+              onPageChanged: _onPageChanged,
+              itemBuilder: (context, index) => const CreditCard(),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < cardList.length; i++)
+                if (_currentCard == i)
+                  DotIndicator(true)
+                else
+                  DotIndicator(false)
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class DotIndicator extends StatefulWidget {
+  final bool isActive;
+  DotIndicator(this.isActive);
+  @override
+  State<DotIndicator> createState() => _DotIndicatorState();
+}
+
+class _DotIndicatorState extends State<DotIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: Container(
+        height: 8.0,
+        width: 8.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4.0),
+            color: widget.isActive ? ThemeColors.black : ThemeColors.grey),
       ),
     );
   }
--- lib/widgets/creditCard.dart
+++ lib/widgets/creditCard.dart
@@ -14,7 +14,7 @@ class _CreditCardState extends State<CreditCard> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(16.0),
       child: Container(
         height: 216,
         width: 380,
@@ -25,6 +25,7 @@ class _CreditCardState extends State<CreditCard> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(
@@ -40,21 +41,43 @@ class _CreditCardState extends State<CreditCard> {
                   ],
                 ),
               ),
-              Column(
-                children: [
-                  Text(
-                    'Magent Black',
-                    style: ThemeStyles.cardDetails,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                          '4756',
-                          style: ThemeStyles.cardDetails,
-                          SvgPicture.asset(''))
-                    ],
-                  )
-                ],
+              Padding(
+                padding: const EdgeInsets.only(left: 24.0, bottom: 32.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Magent Black',
+                      style: ThemeStyles.cardDetails,
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 6.0),
+                          child: Text(
+                            '4756',
+                            style: ThemeStyles.cardDetails,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                          child: SvgPicture.asset('assets/card-dots.svg'),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                          child: SvgPicture.asset('assets/card-dots.svg'),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 6.0),
+                          child: Text(
+                            '9018',
+                            style: ThemeStyles.cardDetails,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               )
             ],
           ),
